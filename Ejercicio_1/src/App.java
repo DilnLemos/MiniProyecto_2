247a7f37c5214a8aee1b37daa9d7bddefb50266d
= <=>
2024-05-15 17:03:15 -0500

    Organización del Primer punto

--- Ejercicio_1/src/App.java
+++ Ejercicio_1/src/App.java
@@ -1,5 +1,9 @@
+package Ejercicio_1.src;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hicimos el punto java fdhjiadhuiao");
     }
+
 }
