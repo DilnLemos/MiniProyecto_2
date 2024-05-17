578869603ea6ed3b6c17c79a60a1bd6c39874bc5
JuanJoseR18 <juan.restrepo.avalo@correounivalle.edu.co>
2024-05-16 23:14:13 -0500

    Se crearon algunos de los menus de la barra superior

--- Ejercicio_3/Recrear_Visual(Nuestro intentoXD)/src/VisualStudiesGUI.java
+++ Ejercicio_3/Recrear_Visual(Nuestro intentoXD)/src/VisualStudiesGUI.java
@@ -15,4 +15,34 @@ public class VisualStudiesGUI {
 
         // Establecer el icono de la ventana
         ImageIcon icono = new ImageIcon("src/VisualLogo.png");
-        ventana.setIconImage(icono.getImage());}}
+        ventana.setIconImage(icono.getImage());
+        
+        // Definir el color de fondo
+        Color color = new Color(24, 20, 28);
+
+        // Crear la barra de menú
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setBackground(color);
+
+        // Crear el menú "Archivo" con sus elementos
+        JMenu archivo = new JMenu("Archivo");
+        archivo.setForeground(Color.GRAY);
+        archivo.add(new JMenuItem("Nuevo"));
+        archivo.add(new JMenuItem("Abrir archivo"));
+        archivo.add(new JMenuItem("Abrir carpeta"));
+        archivo.add(new JMenuItem("Salir"));
+
+        // Crear el menú "Editar" con sus elementos
+        JMenu edit = new JMenu("Editar");
+        edit.setForeground(Color.GRAY);
+        edit.add(new JMenuItem("Copiar"));
+        edit.add(new JMenuItem("Pegar"));
+        edit.add(new JMenuItem("Cortar"));
+
+        // Crear el menú "Seleccion" con sus elementos
+        JMenu seleccion = new JMenu("Seleccion");
+        seleccion.setForeground(Color.GRAY);
+        seleccion.add(new JMenuItem("Seleccionar todo"));
+        seleccion.add(new JMenuItem("Expandir seleccion"));
+        }
+    }
